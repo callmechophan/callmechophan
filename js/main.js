74a12754eecb15327fc37ca825ed7864f0a380ea
Cho Phan <124022452+callmechophan@users.noreply.github.com>
2023-02-17 01:31:11 +0700

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const hamburger_menu = document.querySelector('.hamburger-menu');
-const header_menu = document.querySelector('.header-menu');
-hamburger_menu.addEventListener('click', () => {
-    hamburger_menu.classList.toggle('active');
-    header_menu.classList.toggle('active');
-});
